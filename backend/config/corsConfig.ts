5691931054faa63dfc32cc1e03c02d6c46240cfc
“Nemashaa” <nanemashag@gmail.com>
2025-04-02 15:20:42 +0530

    Updated code

--- backend/config/corsConfig.ts
+++ backend/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: ['http://localhost:5174', 'https://splendorous-longma-229589.netlify.app'], // Allow both local and Netlify origins
+  origin: 'https://splendorous-longma-229589.netlify.app', // Allow both local and Netlify origins
   credentials: true, // Allow cookies
 };
 
