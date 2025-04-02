6000b88e07bd6849757a797561e16e13e2852cd8
“Nemashaa” <nanemashag@gmail.com>
2025-04-02 16:21:06 +0530

    Updated code

--- backend/config/corsConfig.ts
+++ backend/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: 'https://67ed09f497fd2a0062f3a8f7--splendorous-longma-229589.netlify.app',
+  origin: 'https://splendorous-longma-229589.netlify.app/',
   credentials: true, // Allow cookies
 };
 
