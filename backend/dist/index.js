3749c1090b13b4390b973df57ead6b5bd67034c9
“Nemashaa” <nanemashag@gmail.com>
2025-04-02 11:44:55 +0530

    Updated package.json file

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -38,3 +38,4 @@ const PORT = process.env.PORT || 8001; // Backend is running on port 8001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+// This is a test message 
