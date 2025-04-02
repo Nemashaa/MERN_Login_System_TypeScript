58fa11285640f0a928d4eca0c104e8cbd4c42619
“Nemashaa” <nanemashag@gmail.com>
2025-04-02 14:54:20 +0530

    Updated code

--- backend/dist/config/corsConfig.js
+++ backend/dist/config/corsConfig.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
-    origin: 'http://localhost:5174',
+    origin: ['http://localhost:5174', 'https://splendorous-longma-229589.netlify.app'],
     credentials: true, // Allow cookies
 };
 exports.default = corsOptions;
