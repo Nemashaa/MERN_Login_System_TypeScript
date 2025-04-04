068275702da7eb23f5d6dbe51a1838c695ff7f5e
“Nemashaa” <nanemashag@gmail.com>
2025-04-04 11:48:26 +0530

    Updated URLs

--- backend/config/corsConfig.ts
+++ backend/config/corsConfig.ts
@@ -13,8 +13,6 @@ import { CorsOptions } from 'cors';
 
 const allowedOrigins = [
   'https://splendorous-longma-229589.netlify.app', // Remove trailing slash
-  'https://67ed09f497fd2a0062f3a8f7--splendorous-longma-229589.netlify.app',
-  'https://67ed0f9d97fd2a08c8f3a673--splendorous-longma-229589.netlify.app',
 ];
 
 const corsOptions: CorsOptions = {
