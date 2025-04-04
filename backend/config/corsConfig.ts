60b8a9818d8ff0c55c6b443fbdfe7879e1be00c9
“Nemashaa” <nanemashag@gmail.com>
2025-04-04 11:37:35 +0530

    Updated URLs

--- backend/config/corsConfig.ts
+++ backend/config/corsConfig.ts
@@ -1,10 +1,31 @@
-import { CorsOptions } from 'cors';
+/*import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
   origin: ['http://localhost:5174'], // Allow requests from the frontend's origin
   credentials: true, // Allow cookies to be sent with requests
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+};
+
+export default corsOptions;*/
+
+import { CorsOptions } from 'cors';
+
+const allowedOrigins = [
+  'https://splendorous-longma-229589.netlify.app', // Remove trailing slash
+  'https://67ed09f497fd2a0062f3a8f7--splendorous-longma-229589.netlify.app',
+  'https://67ed0f9d97fd2a08c8f3a673--splendorous-longma-229589.netlify.app',
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow cookies
 };
 
 export default corsOptions;
--- frontend/src/utils/axiosInterceptor.tsx
+++ frontend/src/utils/axiosInterceptor.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
-axios.defaults.baseURL = 'http://localhost:8001'; // Backend URL
+//axios.defaults.baseURL = 'http://localhost:8001'; // Backend URL
+axios.defaults.baseURL = 'https://mern-login-system-typescript.onrender.com';
 axios.defaults.withCredentials = true; // Ensure cookies are sent with requests
 
 axios.interceptors.request.use(
